9648f320e34dca2ffc2c1fc1d6ba2afda23900ae
Eddie Peckley <146546451+Eddiepecs@users.noreply.github.com>
2023-12-10 16:21:29 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function toggleSocialLinks() {
-    var socialLinks = document.querySelector('.social-links');
-    socialLinks.classList.toggle('hidden');
+  var socialLinks = document.querySelector('.social-links');
+  socialLinks.classList.toggle('hidden');
+}
+
+document.querySelector('.header-content').addEventListener('click', function (event) {
+  if (!event.target.closest('a')) {
+    toggleSocialLinks();
   }
-  document.querySelector('header').addEventListener('click', toggleSocialLinks);
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+});
